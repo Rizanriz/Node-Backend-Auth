b9472ee1a512b94241d03ee701a0627499de0cd8
Rizanriz <rizanr7777@gmail.com>
2024-11-09 00:38:17 +0530

    ongoing

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,18 +1,20 @@
-const jwt = require ('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 const generateTokenAndSetCookie = (res, userId) => {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: "7d"
-    });
+    const accessToken = jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '15m' }); // Access token valid for 1 hour
+    const refreshToken = jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '7d' }); // Refresh token valid for 7 days
 
-    res.cookie("token", token, {
+    res.cookie("accessToken", accessToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",  
-        sameSite: "strict",
-        maxAge: 3 * 24 * 60 * 60 * 1000  
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 60 * 60 * 1000, // 1 hour
     });
 
-    return token;
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
 };
 
-module.exports = generateTokenAndSetCookie
+module.exports = generateTokenAndSetCookie;
